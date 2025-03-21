c8d4e8b9cceb1f49a66ed9add040deade98b2a88
lichanglin <beelan@yeah.net>
2025-03-21 14:29:52 +0800

    style(commitnorm): :lipstick: rename commitlintConfig.ts

--- src/core/commitnorm/install-necessary.ts
+++ src/core/commitnorm/install-necessary.ts
@@ -3,7 +3,7 @@ import { execSync } from 'child_process'
 import fs from 'fs'
 import { commitlintConfigMjs, husky_commitMsg, husky_preCommit } from './paths'
 import path from 'path'
-import commitlintConfig from './commitlintConfig'
+import commitlintConfig from './commitlint-config'
 
 export default function (execPath: string, devDependencies: Record<string, string>) {
 	console.log(`${loadingIcon} Installing devDependencies`)
