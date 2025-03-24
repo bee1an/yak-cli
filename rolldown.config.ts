9293b3f51ba430cadf35721b60131462c6e19790
lichanglin <beelan@yeah.net>
2025-03-24 10:43:15 +0800

    build: :package: better copy package.json

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'rolldown'
 import fs from 'fs'
 import path from 'path'
 import { srcPath } from './scripts/path'
+import prettier from 'prettier'
 
 export default defineConfig({
 	input: 'src/index.js',
@@ -18,19 +19,24 @@ export default defineConfig({
 		},
 		{
 			name: 'rolldown-plugin-copy',
-			buildEnd() {
+			async buildEnd() {
 				fs.cpSync(path.join(__dirname, 'README.md'), path.join(__dirname, 'dist', 'README.md'))
 
 				const content = fs.readFileSync(path.join(srcPath, 'version.ts'))
-
 				const version = content.toString().match(/\d+\.\d+\.\d+/)![0]
 
-				let pkgContent = fs.readFileSync(path.join(__dirname, 'package.json')).toString()
+				const pkgContent = JSON.parse(
+					fs.readFileSync(path.join(__dirname, 'package.json')).toString()
+				)
 
-				pkgContent = pkgContent.replace(/"version": "\d+\.\d+\.\d+"/, `"version": "${version}"`)
+				pkgContent.version = version
+				delete pkgContent.scripts
 
 				fs.mkdirSync(path.join(__dirname, 'dist'), { recursive: true })
-				fs.writeFileSync(path.join(__dirname, 'dist', 'package.json'), pkgContent)
+				const formatted = await prettier.format(JSON.stringify(pkgContent), {
+					parser: 'json-stringify'
+				})
+				fs.writeFileSync(path.join(__dirname, 'dist', 'package.json'), formatted)
 			}
 		}
 	]
